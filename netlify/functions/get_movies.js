757d401960929cd75af45fe4dd933aedc3839464
Ramin Sedighi <rsedighi@gmail.com>
2024-06-05 09:54:12 -0700

    modified code

--- netlify/functions/get_movies.js
+++ netlify/functions/get_movies.js
@@ -12,10 +12,10 @@ const handler = async (event) => {
         return {
             statusCode: 200,
             body: JSON.stringify(results),
-        }
+        };
     } catch (error) {
-        return { statusCode: 500, body: error.toString() }
+        return { statusCode: 500, body: error.toString() };
     }
-}
+};
 
-module.exports = { handler }
+module.exports = { handler };
