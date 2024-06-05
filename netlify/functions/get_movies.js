10d187eb1694794435404bb5c24d0eaf88a55cec
Ramin Sedighi <rsedighi@gmail.com>
2024-06-05 12:01:24 -0700

    added better style to the page. added poster image to the movies. added pagination to get next 10 results from Atlas

--- netlify/functions/get_movies.js
+++ netlify/functions/get_movies.js
@@ -5,13 +5,19 @@ const mongoClient = new MongoClient(process.env.MONGODB_URI);
 const clientPromise = mongoClient.connect();
 
 const handler = async (event) => {
+    const page = parseInt(event.queryStringParameters.page || '1');
+    const limit = 10;
+    const skip = (page - 1) * limit;
+
     try {
         const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
         const collection = database.collection(process.env.MONGODB_COLLECTION);
-        const results = await collection.find({}).limit(10).toArray();
+        const results = await collection.find({}).skip(skip).limit(limit).toArray();
+        const totalResults = await collection.countDocuments();
+
         return {
             statusCode: 200,
-            body: JSON.stringify(results),
+            body: JSON.stringify({ results, totalResults }),
         };
     } catch (error) {
         return { statusCode: 500, body: error.toString() };
